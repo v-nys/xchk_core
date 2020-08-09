3cbe7766a57b8d71ca30576e269b7462c82481fa
Vincent Nys <vincentnys@gmail.com>
2020-08-09 09:56:01 +0200

    Other option...

--- xchk_core/courses.py
+++ xchk_core/courses.py
@@ -37,6 +37,7 @@ def course_graphs():
             graph.vs[idx]["contentview"] = content
             graph.vs[idx]["label"] = content.uid
             graph.vs[idx]["id"] = content.uid
+            graph.vs[idx]["title"] = content.uid
         # edges toevoegen... lastig, want info is er niet meer
         # zal gewoon nog eens moeten traversen
         for (dependent,dependencies) in courses()[course].structure:
