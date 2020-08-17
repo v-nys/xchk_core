563f94cdfb35025716f8a1d6b4165350620637ba
Vincent Nys <vincentnys@gmail.com>
2020-08-17 12:19:30 +0200

    Paar bedenkingen...

--- xchk_core/context_processors.py
+++ xchk_core/context_processors.py
@@ -1,3 +1,5 @@
 from .courses import courses
+# FIXME: staat hier geloof ik enkel om navbar in te vullen
+# bovendien is courses daar een functie, dus vreemd om die terug te geven
 def courses_context(request):
     return {'courses' : courses}
