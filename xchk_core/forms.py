1b6e5b52f6905e2eac745e640e40b0728a1a4d2a
Vincent Nys <vincentnys@gmail.com>
2020-08-22 10:04:46 +0200

    Om te verkennen.

--- xchk_core/forms.py
+++ xchk_core/forms.py
@@ -21,14 +21,21 @@ class FeedbackForm(forms.ModelForm):
 class RepoSelectionForm(forms.ModelForm):
 
     def __init__(self, owner, *args, **kwargs):
-        containing_node = None
-        if 'containing_node' in kwargs:
-            containing_node = kwargs['containing_node']
-            del(kwargs['containing_node'])
+        print(kwargs.get('uid','uid is niet toegankelijk op deze manier'))
         super(RepoSelectionForm,self).__init__(*args,**kwargs)
+        # FIXME
+        # niet gewoon alles repos van deze user
+        # willen repos waarvoor cursus node in kwestie bevat
+        # denk dat containing_node er niet is...?
+        # repo.course.... kan al niet meer: is gewoon CharField
+        # dus containing_node zal ontbreken
+        # er is wel hidden input uid
+        # Course is geen model, maar kan wel opgevraagd worden via courses.courses()
+        # elke Course heeft een veld structure
+        # en dat is een lijst van tuples (view, dependencies)
         candidate_repos = Repo.objects.filter(user=owner)
-        if containing_node:
-            candidate_repos = [repo for repo in candidate_repos if containing_node in repo.course.contained_nodes()]
+        # if containing_node:
+        #     candidate_repos = [repo for repo in candidate_repos]# if containing_node in repo.course.contained_nodes()]
         numbered_repos = [(repo.id,str(repo)) for repo in candidate_repos]
         self.fields['id'] = forms.ChoiceField(choices=numbered_repos)
 
