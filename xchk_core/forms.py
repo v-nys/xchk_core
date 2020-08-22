90522dd2a395eea49bafe044cfeba0a79912d185
Vincent Nys <vincentnys@gmail.com>
2020-08-22 10:45:39 +0200

    Poging.

--- xchk_core/forms.py
+++ xchk_core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Repo, FeedbackTicket
-from . import strats
+from . import strats, courses
 
 class CheckRequestForm(forms.Form):
 
@@ -21,22 +21,17 @@ class FeedbackForm(forms.ModelForm):
 class RepoSelectionForm(forms.ModelForm):
 
     def __init__(self, owner, *args, **kwargs):
-        print(kwargs.get('uid','uid is niet toegankelijk op deze manier'))
+        uid = kwargs['uid']
         super(RepoSelectionForm,self).__init__(*args,**kwargs)
-        # FIXME
-        # niet gewoon alles repos van deze user
-        # willen repos waarvoor cursus node in kwestie bevat
-        # denk dat containing_node er niet is...?
-        # repo.course.... kan al niet meer: is gewoon CharField
-        # dus containing_node zal ontbreken
-        # er is wel hidden input uid
-        # Course is geen model, maar kan wel opgevraagd worden via courses.courses()
-        # elke Course heeft een veld structure
-        # en dat is een lijst van tuples (view, dependencies)
         candidate_repos = Repo.objects.filter(user=owner)
-        # if containing_node:
-        #     candidate_repos = [repo for repo in candidate_repos]# if containing_node in repo.course.contained_nodes()]
-        numbered_repos = [(repo.id,str(repo)) for repo in candidate_repos]
+        final_candidate_repos = []
+        for repo in candidate_repos:
+            course = courses.courses()[repo.course]
+            for (contentview,_) in course.structure:
+                if contentview.uid == uid:
+                    final_candidate_repos.append(repo)
+                    break
+        numbered_repos = [(repo.id,str(repo)) for repo in final_candidate_repos]
         self.fields['id'] = forms.ChoiceField(choices=numbered_repos)
 
     class Meta:
