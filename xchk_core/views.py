8dbf0bd95f6b535b3039471a979c228cdd224457
Vincent Nys <vincentnys@gmail.com>
2020-09-01 18:08:11 +0200

    slash vergeten en tmux contentviews hebben nog geen titel dus even id gebruiken

--- xchk_core/views.py
+++ xchk_core/views.py
@@ -50,9 +50,9 @@ def new_course_view(request,course_title):
         dependents = dp[1]
         # TODO: add css class indicating whether completed/reachable/...
         # TODO: add URL
-        outcome = f'<li>{dependency.title}<ul>'
+        outcome = f'<li>{dependency.uid}<ul>'
         outcome += ''.join([_dependency_pair_toc_entry(next_lvl,inverted_course) for next_lvl in inverted_course if next_lvl[0] in dependents])
-        outcome += '</ul><li>'
+        outcome += '</ul></li>'
         return outcome
     repo = None
     try:
