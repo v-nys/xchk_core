742c3c4b4f9e1741901eb15c95a9d1aabf8c8109
Vincent Nys <vincentnys@gmail.com>
2020-09-04 18:07:35 +0200

    Juiste voorstelling structuur.

--- xchk_core/views.py
+++ xchk_core/views.py
@@ -62,7 +62,7 @@ def new_course_view(request,course_title):
 
 def course_map_view(request,course_title):
     structure = courses.courses()[course_title].structure
-    id_structure = [dependent.uid : [dependency.uid for dependency in dependencies] for (dependent, dependencies) in structure]
+    id_structure = [(dependent.uid,[dependency.uid for dependency in dependencies]) for (dependent, dependencies) in structure]
     return render(request,'xchk_core/course_map.html',{'graph':json.dumps(id_structure)})
 
 def ulify(tocified,request,course_title,reverse_func=reverse):
